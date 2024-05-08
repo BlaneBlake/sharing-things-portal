caf8aac715070ebe7b96e407168e6667d4d2f7de
Bogdan Anczarski <bogdan@SuperKomputer.L440>
2024-05-08 15:15:46 +0200

    project test

--- sharing_things/authorization/urls.py
+++ sharing_things/authorization/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import ()
+from .views import (Test, HomePageView)
 
 urlpatterns = [
+    path('test/', Test.as_view(), name='test'),
+    path('', HomePageView.as_view(), name='home'),
 
-    ]
+]
--- sharing_things/authorization/views.py
+++ sharing_things/authorization/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+class Test(View):
+    pass
+
+
+# HomePage with Login required mixin (zezwala na dostęp tylko zalogowanego użytkownika)
+class HomePageView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home_page.html')
